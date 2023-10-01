0f1483c9319dc1408a4ba43ccc7b5ef44aabed34
DeboraVicente <dedeboraevelyn256@gmail.com>
2023-10-01 07:47:12 -0300

    createMain

---
+++ OptiStock/OptiStock/main.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <locale.h>
+#include <string.h>
+#include <windows.h>
+
+int menu_perfil();
+void menu_01(nome_pasta);
+void menu_02(nome_pasta);
+void menu_03(nome_pasta);
+void menu_04(nome_pasta);
+void tutorial();
+void cabecalho();
+
+void main(){
+        setlocale(LC_ALL, "Portuguese");
+
+
+
+        return 0;
+    }
+
+void menu_01(){
+    printf("Hello world!\n");
+    return 0;
+}
+
+void menu_02(){
+    printf("Hello world!\n");
+    return 0;
+}
+
+void menu_03(){
+    printf("Hello world!\n");
+    return 0;
+}
+
+void menu_04(){
+    printf("Hello world!\n");
+    return 0;
+}
